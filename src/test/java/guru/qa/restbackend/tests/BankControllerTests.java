1e904cb0529fb0cdc3ac9dd6658d89e98d7ff183
sergejgluhov <arane1223@gmail.com>
2025-11-25 16:09:54 +0500

    updated BankControllerTests

--- src/test/java/guru/qa/restbackend/tests/BankControllerTests.java
+++ src/test/java/guru/qa/restbackend/tests/BankControllerTests.java
@@ -1,56 +1,52 @@
 package guru.qa.restbackend.tests;
 
+import io.qameta.allure.*;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static guru.qa.restbackend.specs.BaseSpecs.*;
+import static guru.qa.restbackend.helpers.TestApiHelper.*;
 import static guru.qa.restbackend.data.TestData.*;
-import static io.restassured.RestAssured.given;
+import static io.qameta.allure.Allure.step;
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Owner("sergeyglukhov")
+@Feature("User Management")
 @DisplayName("Тесты на проверку управления пользователями")
 public class BankControllerTests extends TestBase {
 
     @Test
+    @Story("Получение списка пользователей")
     @DisplayName("Успешное получение списка всех пользователей ")
     void bankControllerAllUsersTest() {
-        Response response = given(baseReqSpec)
-                .get("user/all")
-                .then()
-                .spec(baseRespSpec(200))
-                .extract()
-                .response();
+        Response response = step("Отправить запрос на получение списка пользователей", () ->
+                executeGet("user/all", 200));
 
-        assertThat(response.path("userName").toString()).contains(USERS);
+        step("Проверить список пользователей в ответе с тестовым", () ->
+                assertThat(response.path("userName").toString()).contains(USERS));
     }
 
     @Test
+    @Story("Авторизация")
     @DisplayName("Успешная авторизация")
     void successfulBankControllerAuthTest() {
-        Response response = given(baseReqSpec)
-                .body(CORRECT_AUTH_DATA)
-                .post("user/login")
-                .then()
-                .spec(baseRespSpec(200))
-                .extract()
-                .response();
+        Response response = step("Отправить запрос на авторизацию", () ->
+                executePost("user/login", CORRECT_AUTH_DATA, 200));
 
-        assertThat(response.path("userName").toString()).isEqualTo(CORRECT_AUTH_DATA.getUserName());
+        step("Проверить что авторизация успешна", () ->
+        assertThat(response.path("userName").toString()).isEqualTo(CORRECT_AUTH_DATA.getUserName()));
     }
 
     @Test
+    @Story("Авторизация")
     @DisplayName("Неуспешная авторизация")
     void unsuccessfulBankControllerAuthTest() {
-        Response response = given(baseReqSpec)
-                .body(INCORRECT_AUTH_DATA)
-                .post("user/login")
-                .then()
-                .spec(baseRespSpec(401))
-                .extract()
-                .response();
+        Response response = step("Отправить запрос на авторизацию", () ->
+                executePost("user/login", INCORRECT_AUTH_DATA, 401));
 
-        assertThat(response.path("status").toString()).isEqualTo("401");
-        assertThat(response.path("error").toString()).isEqualTo("Unauthorized");
+        step("Проверить что авторизация неуспешна", () -> {
+            assertThat(response.path("status").toString()).isEqualTo("401");
+            assertThat(response.path("error").toString()).isEqualTo("Unauthorized");
+        });
     }
 }
